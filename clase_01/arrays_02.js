e1097e211ba4bf08d1630bf5ef4c53adebc447dc
teresa <143844055+Teresa2006@users.noreply.github.com>
2023-09-02 20:11:36 +0000

    mi primer commmit

--- clase_01/arrays_02.js
+++ clase_01/arrays_02.js
@@ -3,7 +3,8 @@ console.log('Ejercicio 02...')
 const min = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 const may = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-console.log( may[7] + min[14] + min[11] + min[0] )
+console.log( may[19] + min[4] + min[17] + min[4] + min[18] + min[0] )
+console.log( may[7] + min[14] + min[13] + min[14] + min[17])
 
 
 // Ahora haz log tu nombe en la consola en un sola linea.
--- clase_01/arrays_04.js
+++ clase_01/arrays_04.js
@@ -1,17 +1,17 @@
 console.log('Ejercicio 04...')
 
 
+const años = [ 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010]
 const meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
 const fechas = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31] 
 const dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
 
+const size_años = años.length
 const size_meses = meses.length  // Tamaño del array de meses
 const size_fechas = fechas.length  // Tamaño del array de fechas
 const size_dias = dias.length  // Tamaño del array de dias
 
-for (let i = 0; i < size_meses; i= i +1 ) {
-  console.log(meses[i])
-}
-
-
+for (let i = 2; i < size_meses; i= i + 11 ) {console.log(meses[i])}
+for (let i = 12; i < size_fechas; i= i + 30 ) { console.log(fechas[i])}
+for (let i = 4; i < size_dias; i= i + 6 ) { console.log(dias[i])}
 // Ahora recorre el array de "dias" usando el loop for, como el ejemplo de arriba
